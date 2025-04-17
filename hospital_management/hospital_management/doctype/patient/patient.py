534010ce9df1f9650b744c8d7829e640c500501e
Babar <babar88sgd@gmail.com>
2025-04-17 12:42:09 +0500

    Updated client scripts and custom changes

--- hospital_management/hospital_management/doctype/patient/patient.py
+++ hospital_management/hospital_management/doctype/patient/patient.py
@@ -3,18 +3,17 @@ from frappe.model.document import Document
 
 class Patient(Document):
     pass
-# Path: custom_app/custom/doctype/patient/patient.py
 
-@frappe.whitelist()
-def patient_name_query(doctype, txt, searchfield, start, page_len, filters):
-    return frappe.db.sql("""
-        SELECT 
-            name, patient_name
-        FROM 
-            `tabPatient`
-        WHERE 
-            patient_name LIKE %(txt)s
-        LIMIT 20
-    """, {
-        "txt": f"%{txt}%"
-    })
+def get_dashboard_data():
+    return {
+        'transactions': [
+            {
+                'label': __('Connections'),
+                'items': [
+                    'Patient Appointment',
+                    'Billing',
+                    'Prescription',
+                ]
+            }
+        ]
+    }
